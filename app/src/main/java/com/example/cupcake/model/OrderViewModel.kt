f9b8dc215e2c62f6cd1d6793add228864e67eaf7
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-06-03 15:48:20 +0530

    creating viewmodel with attributes and functions

---
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.cupcake.model
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class OrderViewModel : ViewModel() {
+
+    private val _quantity = MutableLiveData<Int>(0)
+    val quantity : LiveData<Int> get() =_quantity
+
+    private val _flavor = MutableLiveData<String>()
+    val flavor : LiveData<String> get() =_flavor
+
+    private val _date = MutableLiveData<String>()
+    val date : LiveData<String> get() = _date
+
+    private val _price = MutableLiveData<Double>(0.0)
+    val price : LiveData<Double> get() =_price
+
+    fun setQuantity(noOfCupcakes : Int){
+        _quantity.value = noOfCupcakes
+    }
+
+    fun setFlavor(flavorDeatils : String){
+        _flavor.value = flavorDeatils
+    }
+    fun setDate(pickUpDate : String){
+        _date.value = pickUpDate
+    }
+
+}
